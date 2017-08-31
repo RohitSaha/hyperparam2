c741e4f7d43e086a1487395227d61851398c6acd
nathanlmeyers <nathanlmeyers@gmail.com>
2017-08-31 14:07:29 -0400

    update

--- gpyopt/test.py
+++ gpyopt/test.py
@@ -3,10 +3,11 @@ from GPyOpt.methods import BayesianOptimization
 import numpy as np
 
 # --- Define your problem
-def f(x,y): return (6*x-2)**2*np.sin(12*x-4)+y**2
-bounds = [(0,1),(-5,-2)]
+def my_func(x,y): return (6*x-2)**2*np.sin(12*x-4)+y**2
+bounds = [{'name': 'x','type': 'continuous', 'domain': (0,1)},
+          {'name': 'y','type': 'continuous', 'domain': (0,1)}]
 
 # --- Solve your problem
-myBopt = BayesianOptimization(f=f, bounds=bounds)
+myBopt = BayesianOptimization(f=my_func, domain=bounds)
 myBopt.run_optimization(max_iter=15)
 myBopt.plot_acquisition()
